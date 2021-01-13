ca3c02a00af0986f06a0899a0ca7a6b3af5db42e
Aravindh Kumar <aravindhkumar23@gmail.com>
2021-01-13 13:59:53 +0530

    Get version info widget added with optional params, example source added.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## [0.0.1] - TODO: Add release date.
+## [0.0.1] - Initial release.
 
-* TODO: Describe initial release.
+* A new Flutter package to create a widget that shows app version information.
--- lib/app_version_info.dart
+++ lib/app_version_info.dart
@@ -1,7 +1,35 @@
 library app_version_info;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:get_version/get_version.dart';
+
+class AppVersionInfo {
+  static Widget getVersionInfo({String prefixText, TextStyle textStyle}) {
+    return FutureBuilder<String>(
+      future: getVersionInfoString(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.hasData) {
+          return new Container(
+            child: new Text(
+              '${prefixText ?? 'App Version :'} ${snapshot.data}',
+              style: textStyle ?? new TextStyle(color: Colors.black),
+            ),
+          );
+        }
+        return new Container();
+      },
+    );
+  }
+
+  static Future<String> getVersionInfoString() async {
+    try {
+      final String projectVersion = await GetVersion.projectVersion;
+      final String projectCode = await GetVersion.projectCode;
+      final versionString = '$projectVersion($projectCode)';
+      return versionString;
+    } catch (_) {
+      return '';
+    }
+  }
 }
